e789d9b18f4edb14d238a8610cf557f830f84744
kskw0000 <kskw2525@gmail.com>
2023-06-20 02:51:41 +0900

    2列にします

---
+++ .history/frontend/src/components/pages/NurseryList_20230620023159.js
@@ -0,0 +1,73 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { Link } from 'react-router-dom';
+import SearchBox from '../parts/SearchBox';
+import SelectRegion from '../parts/SelectRegion'; 
+
+function NurseryList({ setSelectedNursery }) {
+
+  const [nurseries, setNurseries] = useState([]);
+  const [searchText, setSearchText] = useState('');
+  const [selectedRegion, setSelectedRegion] = useState('全地域');
+
+  useEffect(() => {
+    axios.get(`${process.env.REACT_APP_SERVER_ROOT_URL}/nurseries`)
+      .then(res => {
+        const nurseries = res.data
+        setNurseries(nurseries.filter(nurse => !!nurse.name))
+      })
+      .catch(err => console.log(err));
+  }, []);
+
+  const filteredNurseries = nurseries.filter(nursery => 
+    (nursery.name && nursery.name.toLowerCase().includes(searchText.toLowerCase())) &&
+    (selectedRegion === '全地域' || nursery.region === selectedRegion)
+  );
+
+  const firstRowNurseries = filteredNurseries.slice(0, filteredNurseries.length - 10);
+  const secondRowNurseries = filteredNurseries.slice(filteredNurseries.length - 10);
+
+  const searchAndSelectStyle = { 
+    display: 'flex', 
+    flexDirection: 'column', 
+    alignItems: 'center',
+    margin: '20px 0'
+  };
+
+  return (
+    <div>
+      <h2>保育園一覧</h2>
+      <div style={searchAndSelectStyle}>
+        <SearchBox onSearch={setSearchText} />
+        <SelectRegion onSelect={setSelectedRegion} />
+      </div>
+      <div className="card-container"> 
+        {firstRowNurseries.map((nursery, index) => (
+          <Link key={nursery.id} to={`/nursery/${nursery.id}`} style={{ textDecoration: 'none', color: 'inherit' }}>
+            <div className="card">
+              <div className="card-image">
+                <img src={nursery.thumbnail} alt={nursery.name} />
+                <p>{nursery.name}</p>
+              </div>
+            </div>
+          </Link>
+        ))}
+      </div>
+      <h2>新着保育園</h2>
+      <div className="card-container"> 
+        {secondRowNurseries.map((nursery, index) => (
+          <Link key={nursery.id} to={`/nursery/${nursery.id}`} style={{ textDecoration: 'none', color: 'inherit' }}>
+            <div className="card">
+              <div className="card-image">
+                <img src={nursery.thumbnail} alt={nursery.name} />
+                <p>{nursery.name}</p>
+              </div>
+            </div>
+          </Link>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default NurseryList;
