51a23a78dd80ae86cff96e3d9e9fd82244c785da
kskw0000 <kskw2525@gmail.com>
2023-06-20 00:47:17 +0900

    local history

--- server/index.js
+++ server/index.js
@@ -1,83 +1,207 @@
-import React, { useState, useEffect } from 'react';
-import { useParams } from 'react-router-dom';
-import axios from 'axios';
-import ReviewForm from './ReviewForm';
-import ReviewCard from '../parts/ReviewCard';
-import styles from '../styles/NuseryDeep.module.css'; // Add this line
-import reviewStyles from '../styles/ReviewCard.module.css'; // 追加
-import '../styles/App.css';
+equire('dotenv').config();
 
-function Nuserydeep() {
-  const { id } = useParams();
-  const [nursery, setNursery] = useState(null);
-  const [reviews, setReviews] = useState([]);
+const express = require('express');
+const sqlite3 = require('sqlite3').verbose();
+const bcrypt = require('bcrypt');
+const app = express();
+const port = 3001;
+const cors = require('cors');
+const multer = require('multer');
+const jwt = require('jsonwebtoken');
 
-  useEffect(() => {
-    // 保育園の詳細情報を取得
-    axios.get(`${process.env.REACT_APP_SERVER_ROOT_URL}/nurseries/${id}`)
-      .then(res => {
-        setNursery(res.data);
-      })
-      .catch(err => console.log(err));
-  }, [id]);
+function authenticateToken(req, res, next) {
+  const authHeader = req.headers['authorization'];
+  const token = authHeader && authHeader.split(' ')[1];
 
-  useEffect(() => {
-    // その保育園のレビューを取得
-    axios.get(`${process.env.REACT_APP_SERVER_ROOT_URL}/nurseries/${id}/reviews`)
-      .then(res => {
-        setReviews(res.data || []);
-      })
-      .catch(err => console.log(err));
-  }, [id]);
+  if (token == null) return res.sendStatus(401); // if there isn't any token
 
-
-  const handleReviewSubmit = (review) => {
-    const reviewWithNurseryId = { ...review, nurseryId: id };
-    axios.post(`${process.env.REACT_APP_SERVER_ROOT_URL}/nurseries/${id}/reviews`, review)
-      .then(res => {
-        if (res.status === 200) {
-          setReviews(prevReviews => [...prevReviews, res.data]);
-        } else {
-          console.log('Error: ', res);
-        }
-      })
-      .catch(err => console.error(err));
-  };
-
-  return (
-    <div className={styles.container}> {/* Add className */}
-      {nursery ? (
-        <div>
-          <div className={styles.imgBox}>
-            <img src={nursery.image} alt="Nursery" className={styles.img} />
-          </div>
-          <div className="detailsBox">
-            <h2 className="detailsText">{nursery.name}</h2>
-            <p className="detailsText">{nursery.location}</p>
-            <p className="detailsText">{nursery.type}</p>
-            {/* Add other details as needed */}
-          </div>
-        </div>
-      ) : (
-        <p>保育園情報 Loading...</p>
-      )}
-
-    <ReviewForm nurseryId={id} onReviewSubmit={handleReviewSubmit} />
-
-    <div className={reviewStyles.reviewsContainer}> {/* Add this line */}
-      {reviews.length > 0 ? (
-        <div>
-          <h3 className={reviewStyles.reviewTitle}>口コミ</h3>
-          {reviews.map((review, index) => (
-            <ReviewCard key={index} review={review} />
-          ))}
-        </div>
-      ) : (
-        <p>口コミはまだありません。</p> // reviewsが存在しない場合のメッセージ
-      )}
-    </div>
-    </div>
-  );
+  jwt.verify(token, process.env.SECRET_KEY, (err, user) => {
+    if (err) return res.sendStatus(403);
+    req.user = user;
+    next();
+  });
 }
 
-export default Nuserydeep;
+
+const db = new sqlite3.Database('./reviews.db', (err) => {
+  if (err) {
+    console.error(err.message);
+  }
+  console.log('Connected to the reviews database.');
+});
+
+db.run(`
+  CREATE TABLE IF NOT EXISTS users (
+    id INTEGER PRIMARY KEY,
+    email TEXT UNIQUE,
+    password TEXT
+  )
+`, err => {
+  if (err) {
+    console.error(err.message);
+  }
+  console.log('Created users table.');
+});
+
+app.use(express.json());
+app.use(cors());
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+// Set up multer
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, 'uploads/')
+    },
+    filename: function (req, file, cb) {
+      cb(null, file.fieldname + '-' + Date.now() + '.jpg')
+    }
+  });
+
+const upload = multer({ storage: storage });
+
+app.post('/register', (req, res) => {
+  const { email, password } = req.body;
+  const saltRounds = 10;
+
+  // Hash the password
+  bcrypt.hash(password, saltRounds, function(err, hash) {
+      if (err) {
+          return res.status(500).json({ error: "Internal server error" });
+      }
+      db.run('INSERT INTO users(email, password) VALUES(?,?)', [email, hash], function(err) {
+          if (err) {
+            return console.log(err.message);
+          }
+          res.json({ id: this.lastID });
+      });
+  });
+});
+
+// Login existing users
+app.post('/login', (req, res) => {
+  const { email, password } = req.body;
+
+  db.get('SELECT * FROM users WHERE email = ?', [email], (err, row) => {
+    if (err) {
+      return res.status(500).json({ error: "Internal server error" });
+    }
+    if (!row) {
+      return res.status(401).json({ error: "Invalid email or password" });
+    }
+
+    bcrypt.compare(password, row.password, function(err, result) {
+      if (err) {
+        return res.status(500).json({ error: "Internal server error" });
+      }
+      if (result) {
+        // TODO: You may want to create a token and return it here.
+        // Create a JWT
+        const token = jwt.sign({ id: row.id }, process.env.SECRET_KEY, { expiresIn: '1h' }); // Replace 'your_secret_key' with your actual secret key
+        res.json({ message: "Login successful", token: token });
+      } else {
+        res.status(401).json({ error: "Invalid email or password" });
+      }
+    });
+  });
+});
+
+app.post('/nurseries', upload.single('image'), (req, res) => {
+    const { name, location, type } = req.body;
+    const image = req.file ? req.file.filename : null;
+
+    db.run('INSERT INTO nurseries(name, location, type, image) VALUES(?,?,?,?)', [name, location, type, image], function(err) {
+        if (err) {
+          return console.log(err.message);
+        }
+        res.json({ id: this.lastID });
+    });
+});
+
+// 保育園情報の登録
+app.post('/nurseries', (req, res) => {
+    const { name, location, type } = req.body;
+
+    db.run('INSERT INTO nurseries(name, location, type) VALUES(?,?,?)', [name, location, type], function(err) {
+        if (err) {
+          return console.log(err.message);
+        }
+        res.json({ id: this.lastID });
+    });
+});
+
+// 保育園情報の編集
+app.put('/nurseries/:id', (req, res) => {
+    const { name, location, type } = req.body;
+
+    db.run('UPDATE nurseries SET name = ?, location = ?, type = ? WHERE id = ?', [name, location, type, req.params.id], function(err) {
+        if (err) {
+          return console.log(err.message);
+        }
+        res.json({ changes: this.changes });
+    });
+});
+
+// 保育園情報の削除
+app.delete('/nurseries/:id', (req, res) => {
+    db.run('DELETE FROM nurseries WHERE id = ?', req.params.id, function(err) {
+        if (err) {
+          return console.log(err.message);
+        }
+        res.json({ changes: this.changes });
+    });
+});
+
+// 保育園情報の取得
+app.get('/nurseries', (req, res) => {
+    db.all('SELECT * FROM nurseries', [], (err, rows) => {
+        if (err) {
+            throw err;
+        }
+        res.json(rows);
+    });
+});
+
+// 保育園の詳細情報を取得するエンドポイント
+app.get('/nurseries/:id', (req, res) => {
+    db.get('SELECT * FROM nurseries WHERE id = ?', [req.params.id], (err, row) => {
+        if (err) {
+            return console.error(err.message);
+        }
+        res.json(row);
+    });
+});
+
+// その保育園のレビューを取得するエンドポイント
+app.get('/nurseries/:id/reviews', (req, res) => {
+    const { id: nurseryId } = req.params;
+
+    db.all('SELECT * FROM reviews WHERE nurseryId = ?', [nurseryId], (err, rows) => {
+      if (err) {
+        return console.error(err.message);
+      }
+      res.json(rows);
+    });
+});
+
+// 新たなレビューを作成するエンドポイント
+app.post('/nurseries/:id/reviews',authenticateToken, (req, res) => {
+    const { yard, rating, comment } = req.body;
+    const { id: nurseryId } = req.params;
+
+    const sql = 'INSERT INTO reviews(nurseryId, yard, rating, comment) VALUES(?, ?, ?, ?)';
+    const params = [nurseryId, yard, rating, comment];
+
+    db.run(sql, params, function (err) {
+      if (err) {
+        return console.error(err.message);
+      }
+      res.json({ id: this.lastID, nurseryId, yard, rating, comment });
+    });
+});
+
+app.listen(port, () => {
+    console.log(`Server is running at :${port}`);
+});
